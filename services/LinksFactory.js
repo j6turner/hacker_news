625ffc2c7799987c024585a0031eb26bdd7644fc
Andy Uppendahl and J. TURNER <a.uppendahl@gmail.com>
2015-02-26 14:58:06 -0800

    Add upvote functionality for all links.

--- services/LinksFactory.js
+++ services/LinksFactory.js
@@ -8,12 +8,21 @@ hackerNews.factory("LinksFactory", function LinksFactory() {
       url: factory.linkURL,
       id: factory.links.length + 1,
       upvotesCount: 0,
-      upvote: function() { ++upvotesCount; }
+      upvote: function() { ++(this.upvotesCount); },
       comments: []
     });
 
     factory.linkTitle = null;
     factory.linkURL = null;
+  };
+
+  factory.findLinkByID = function(id) {
+    for (var i = 0; i < factory.links.length; ++i) {
+      if (factory.links[i].id == id) {
+        return factory.links[i];
+      }
+    }
+    return null;
   };
 
   return factory;
