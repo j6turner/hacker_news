207a08c797fd22e1830b7fb12ccd5ae3ed250643
Andy Uppendahl and J. TURNER <a.uppendahl@gmail.com>
2015-02-26 13:54:53 -0800

    Add upvote function.

--- services/LinksFactory.js
+++ services/LinksFactory.js
@@ -7,9 +7,13 @@ hackerNews.factory("LinksFactory", function LinksFactory() {
       title: factory.linkTitle,
       url: factory.linkURL,
       id: factory.links.length + 1,
+      upvotesCount: 0,
+      upvote: function() { ++upvotesCount; }
       comments: []
     });
+
     factory.linkTitle = null;
+    factory.linkURL = null;
   };
 
   return factory;
