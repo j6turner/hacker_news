fdc07d00382713a5a2fa80bfc47d42eeac916f9f
J. TURNER <turner.j6@gmail.com>
2015-03-02 02:13:17 -0800

    Establish 'addComment' method.

--- controllers/CommentsController.js
+++ controllers/CommentsController.js
@@ -3,5 +3,15 @@ hackerNews.controller("CommentsCtrl",
   $scope.links = LinksFactory.links;
   $scope.LinksFactory = LinksFactory;
   $scope.link = LinksFactory.findLinkByID($stateParams.linkId);
-
+  $scope.addComment = function() {
+    $scope.link.comments.push({
+      name: $scope.userName,
+      comment: $scope.comment,
+      upvotesCount: 0,
+      agingFactor: 0,
+      id: $scope.link.comments.length + 1
+    });
+  $scope.userName = null;
+  $scope.comment = null;
+  }
 });
